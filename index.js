e2d6053af4bbdffe2b63f185133ebec163a67e24
Gyöngyvér Szanyi <gyongyver.szanyi@gmail.com>
2025-07-02 21:39:53 +0200

    remove CORS

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 
-const cors = require('cors')
-app.use(cors())
-
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan('tiny', {
