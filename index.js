3eb35f0824592246b2544165dff6c57ca917613c
Gyöngyvér Szanyi <gyongyver.szanyi@gmail.com>
2025-07-07 17:57:58 +0200

    Part 3c: 3.14 phonebook database step2 - save numbers to the database

--- index.js
+++ index.js
@@ -52,13 +52,9 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -77,7 +73,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const randomId = () => Math.floor(Math.random() * 100000).toString()
+//const randomId = () => Math.floor(Math.random() * 100000).toString()
 
 app.post('/api/persons/', (request, response) => {
   const body = request.body
@@ -86,19 +82,21 @@ app.post('/api/persons/', (request, response) => {
     return response.status(400).json({
       error: 'Missing information: please give both name and phone number!'
     })
-  } else if (persons.find(person => person.name === body.name)) {
+  } 
+  /*else if (persons.find(person => person.name === body.name)) {
     return response.status(400).json({
       error: `${body.name} is already in the phonebook`
     })
-  }
+  }*/
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: randomId(),
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 const PORT = process.env.PORT || 3001
