03c5913accadf1b4ae1971614cb0a33a655d8587
Gyöngyvér Szanyi <gyongyver.szanyi@gmail.com>
2025-07-08 18:52:25 +0200

    Part 3c: 3.19* phonebook database step7 - expand validation and modify frontend to display error message when validation error occurs

--- index.js
+++ index.js
@@ -85,30 +85,32 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
+/*   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'Missing information: please give both name and phone number!'
     })
-  }
+  } */
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body
-  console.log('request: ', request.body)
+  console.log('request: ', request.body, 'id: ', request.params.id)
 
-  Person.findById(request.params.id)
+  /*Person.findById(request.params.id)
     .then((person) => {
       if (!person) {
         return response.status(404).end()
@@ -120,6 +122,12 @@ app.put('/api/persons/:id', (request, response, next) => {
         response.json(updatedPerson)
       })
     })
+    .catch(error => next(error))*/
+
+    Person.findByIdAndUpdate(request.params.id, {number: number}, { runValidators: true })
+    .then((originalPerson) => {
+        Person.findById(originalPerson._id).then((person)=>response.json(person))
+      })
     .catch(error => next(error))
 })
 
@@ -137,6 +145,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
